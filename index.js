67dd4185be1205965564c846ba0f9f7e6fb37ba1
Sterling <sterlinglc123@gmail.com>
2022-09-20 21:23:31 -0700

    working on index file and classes (lib)

--- index.js
+++ index.js
@@ -26,9 +26,55 @@ const AskManager = () => {
         },
 
         {
-    
+            type: "input",
+            message: "What is the manager's name?",
+            name: "managerName",
+            // validate: (Function) Receive the user input and answers hash. Should return true if the value is valid, and an error message (String) otherwise. If false is returned, a default error message is provided.
+            validate: managerInput => {
+                if(managerInput){
+                    return true;
+                }
+                else{
+                    console.log("Please enter manager's name!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "managerName",
+            // validate: (Function) Receive the user input and answers hash. Should return true if the value is valid, and an error message (String) otherwise. If false is returned, a default error message is provided.
+            validate: managerInput => {
+                if(managerInput){
+                    return true;
+                }
+                else{
+                    console.log("Please enter manager's name!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "managerName",
+            // validate: (Function) Receive the user input and answers hash. Should return true if the value is valid, and an error message (String) otherwise. If false is returned, a default error message is provided.
+            validate: managerInput => {
+                if(managerInput){
+                    return true;
+                }
+                else{
+                    console.log("Please enter manager's name!")
+                    return false;
+                }
+            }
         }
     ])
+    .then((response) => {
+        console.log(response);
+    })
 
 }
 
+AskManager();
