fdc420a23116c24832309c427d47e55e1fa33d59
Sterling <sterlinglc123@gmail.com>
2022-09-20 20:46:26 -0700

    inital index info

---
+++ index.js
@@ -0,0 +1,34 @@
+//run application 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//need to enter team manager's name, employee id, email address, and office number
+
+//need to save manager info into variable for printing in html later
+const AskManager = () => {
+    inquirer    
+    .prompt([
+        {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "managerName",
+            // validate: (Function) Receive the user input and answers hash. Should return true if the value is valid, and an error message (String) otherwise. If false is returned, a default error message is provided.
+            validate: managerInput => {
+                if(managerInput){
+                    return true;
+                }
+                else{
+                    console.log("Please enter manager's name!")
+                    return false;
+                }
+            }
+
+        },
+
+        {
+    
+        }
+    ])
+
+}
+
