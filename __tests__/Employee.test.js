300607bef6a531cf1ef37a5dacb3119f3c87ee8c
Sterling <sterlinglc123@gmail.com>
2022-09-21 19:30:51 -0700

    working on tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,16 @@
 // Intializiation tests
+const Employee = require("../Employee");
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should return an object containing a name")
+        const object = new Employee();
+
+        expect("name" in object).toEqual(true);
+    });
+
+    it("should set 'name' ")
+})
 
     // Testing the constructor
 
