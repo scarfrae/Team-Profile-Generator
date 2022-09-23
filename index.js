69725df3667ec4fded0b6822fc190c6b164738f6
Sterling <sterlinglc123@gmail.com>
2022-09-23 10:54:03 -0700

    working on index.js and page.template

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //require lib
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+
+var EmployeeInfoList = [];
 
 //need to enter team manager's name, employee id, email address, and office number
 //need to save manager info into variable for printing in html later
@@ -69,30 +74,58 @@ const AskManager = () => {
                     return false;
                 }
             }
-        },
+        }
+    ])
+    .then((response) => {
+        const manager = new Manager(response.managerName, response.ID, response.managerEmail, response.managerOfficeNum);
+        EmployeeInfoList.push(manager);
+        OptionMenu();
+    })
+
+}
+
+const OptionMenu = () => {
+    inquirer    
+    .prompt([
         {
             type: "list",
             message: "Which team member do you want to add?",
             name: "memberOption",
-            choices: ['Manager', 'Engineer', 'Intern'],
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish building your team'],
             // validate: (Function) Receive the user input and answers hash. Should return true if the value is valid, and an error message (String) otherwise. If false is returned, a default error message is provided.
             validate: TeamMemberInput => {
                 if(TeamMemberInput){
                     return true;
                 }
                 else{
-                    console.log("Please enter a positive number greater than 0!")
+                    console.log("Please choose an option!")
                     return false;
                 }
             }
         }
 
-
     ])
     .then((response) => {
+        if(response.memberOption == 'Add an Engineer'){
+            AddEngineer();
+        }
+        else if(response.memberOption == 'Add an Intern'){
+            AddIntern();
+        }
+        else {
+            FinishedTeam();
+        }
         console.log(response);
     })
 
 }
 
+//add engineer and add intern are really similar to addManager function
+//once engineer and intern occur call option menu
+//when selecting FinishedTeam write html file
+//invoke whichever one I export from page.template(creating html one) and pass EmployeeInfoList
 AskManager();
+
+const AskEngineer = () => {
+    console.log(Hi)
+}
--- src/page.template.js
+++ src/page.template.js
@@ -6,3 +6,55 @@
 //push those cards to array of html snipits
 //take array and inject it into final html template
 //return html template
+
+const ManagerCard = (data) => {
+    return`
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${data.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data.getRole()}</h6>
+            <ul class="card-text"><li>"ID:" ${data.getId()}</li>
+            <li>"Email:" ${data.getEmail()}</li>
+            <li>"Office Number:" ${data.getOfficeNum()}</li></ul>
+        </div>
+    </div>
+    ` 
+}
+
+const EngineerCard = (data) => {
+    return`
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${data.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data.getRole()}</h6>
+            <ul class="card-text"><li>"ID:" ${data.getId()}</li>
+            <li>"Email:" ${data.getEmail()}</li>
+            <li>"Office Number:" ${data.getGithub()}</li></ul>
+        </div>
+    </div>
+    ` 
+}
+
+const InternCard = (data) => {
+    return`
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${data.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data.getRole()}</h6>
+            <ul class="card-text"><li>"ID:" ${data.getId()}</li>
+            <li>"Email:" ${data.getEmail()}</li>
+            <li>"Office Number:" ${data.getSchool()}</li></ul>
+        </div>
+    </div>
+    ` 
+}
+
+const FilterArray = (EmployeeArray) => {
+    const result = words.filter(word => word.length > 6);
+// 1. split each employee based on type it is
+// 2. take array of employee type and send each element through html card function
+// 3. take all arrays and put into <body> of final html template
+// 4. write the function to html
+}
+
+//export function that creates final html
